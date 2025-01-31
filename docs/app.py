6e99088ef8b1a840dff1c73c24ddcddbc55e9d2a
Takayuki-Suda <takayuki.suda.job@gmail.com>
2025-01-31 17:26:08 +0900

    Template Refactoring: 詳細設計のHTMLファイルを共通化
    - base.html を作成し、他の詳細設計ページ（task-management/index.html, data-model.html, sample-code.html）で拡張
    - Flaskの render_template を使用して、共通のレイアウトとコンテンツブロックを適用
    - 各ページを個別にレンダリングするルートを作成し、コードの再利用性を向上

--- docs/app.py
+++ docs/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='static', template_folder='html')
 
 @app.route('/')
 def home():
@@ -9,6 +9,19 @@ def home():
 @app.route('/<path:path>')
 def static_files(path):
     return send_from_directory('html', path)
+
+@app.route('/detailed-design/task-management/index.html')
+def detailed_design_index():
+    return render_template('detailed-design/task-management/index.html')
+
+@app.route('/detailed-design/task-management/data-model.html')
+def data_model():
+    return render_template('detailed-design/task-management/data-model.html')
+
+@app.route('/detailed-design/task-management/sample-code.html')
+def sample_code():
+    return render_template('detailed-design/task-management/sample-code.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
