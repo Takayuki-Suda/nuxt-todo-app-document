6db2d418da0a9895e61b7bb830ba33686404cecf
Takayuki-Suda <takayuki.suda.job@gmail.com>
2025-02-03 10:20:08 +0900

    refactor: テンプレートのレンダリングと動的ルート生成の簡素化
    - 動的テンプレートパスを処理する共通レンダリング関数 `render_dynamic_template` を追加
    - `task-management` と `featureA` 用のルートを動的に生成する `add_dynamic_routes` 関数を導入
    - ルート固有の関数を削除し、タスク管理と featureA のテンプレートに対して動的な処理を活用

--- docs/app.py
+++ docs/app.py
@@ -11,22 +11,36 @@ def home():
 def static_files(path):
     return send_from_directory('html', path)
 
-def render_task_management(template_path, filename):
-    return render_template(f'{template_path}/task-management/{filename}')
+# 共通のレンダリング関数
+def render_dynamic_template(base_path, template_path, filename):
+    return render_template(f'{base_path}/{template_path}/{filename}')
 
-# 動的にルートを生成
+# 動的にルートを生成する共通関数
+def add_dynamic_routes(routes, base_paths, view_func):
+    """動的ルートを追加する共通処理"""
+    for base_path in base_paths:
+        for route in routes:
+            app.add_url_rule(
+                f'/{route}/{base_path}/<filename>',
+                endpoint=f'{base_path}_{route.replace("/", "_").replace("-", "_")}',
+                view_func=partial(view_func, route, base_path)
+            )
+
+# ルートの定義
 task_management_routes = [
     'detailed-design',
     'requirements_and_design',
     'technology-details'
 ]
 
-for route in task_management_routes:
-    app.add_url_rule(
-        f'/{route}/task-management/<filename>',
-        endpoint=f'task_management_{route.replace("-", "_")}', 
-        view_func=partial(render_task_management, route)
-    )
+# base_pathのリスト
+base_paths = [
+    'task-management',
+    'featureA'
+]
+
+# 動的にルートを追加
+add_dynamic_routes(task_management_routes, base_paths, render_dynamic_template)
 
 if __name__ == '__main__':
     app.run(debug=True)
